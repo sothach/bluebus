a047cae99ed0560f1de02b8d4f8346dd81349f1d
sothach <phillips.roy@gmail.com>
2015-11-26 11:49:19 +0000

    Update SBConfig.scala

--- SBConfig.scala
+++ SBConfig.scala
@@ -2,6 +2,6 @@ import java.time.Duration
 
 case class SBusConfig(rootUri: String, namespace: String, tokenLease: Duration, timeout: Duration,
                        queueName: String, sasKeyName: String, sasKey: String) {
-  val endpoint = s"https://$namespace.servicebus.windows.net/$queueName/messages"
+  val endpoint = s"https://$namespac$rootUri/$queueName/messages"
   def sasToken = SASAuthFactory.createToken(endpoint, sasKeyName, sasKey, tokenLease)
 }
