7bb75d14e734be733f72ddfa7bbdc27ddd7ee79b
sothach <phillips.roy@gmail.com>
2015-11-26 11:47:44 +0000

    Create Sample.scala

---
+++ Sample.scala
@@ -0,0 +1,25 @@
+object Sample {
+
+  val inboundBus = SBBusConfig(
+    rootUri=".servicebus.windows.net", 
+    namespace="yourServiceNamspace",
+    tokenLease=Duration.ofMinutes(5), 
+    timeout=Duration.ofSeconds(30),
+    queueName="queueName",
+    sasKeyname="RootManageSharedAccessKey, 
+    sasKey="yourKey")
+    
+  val incomingService = new ServiceBusClient(FDSConfig.inboundBus)
+    
+  def receiveMessages(client: ServiceBusClient, handler: (String) => Unit): Unit = {
+    client.receive onComplete {
+      case Success(message) =>
+        receive(message)
+        receiveMessages(client, handler)
+      case Failure(t) =>
+        Logger.warn(s"SB receive error: ${t.getMessage}")
+    }
+  }
+  
+  def test = receiveMessages(incomingService, (msg: String) => println(msg))
+}
