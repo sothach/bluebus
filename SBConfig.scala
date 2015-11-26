2618ad980a89d4a7c61fab0859eb9ac329461f8a
sothach <phillips.roy@gmail.com>
2015-11-26 12:10:08 +0000

    Update SBConfig.scala

--- SBConfig.scala
+++ SBConfig.scala
@@ -3,6 +3,6 @@ import java.time.Duration
 case class SBusConfig(rootUri: String, namespace: String, tokenLease: Duration, timeout: Duration,
                        queueName: String, sasKeyName: String, sasKey: String, 
                        contentType: String = "application/json;charset=utf-8") {
-  val endpoint = s"https://$namespac$rootUri/$queueName/messages"
+  val endpoint = s"https://$namespace$rootUri/$queueName/messages"
   def sasToken = SASAuthFactory.createToken(endpoint, sasKeyName, sasKey, tokenLease)
 }
