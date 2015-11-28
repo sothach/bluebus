5f6fe42ade875ae09f68a2ad6d8b0c4ea9a483e8
sothach <phillips.roy@gmail.com>
2015-11-28 14:17:07 +0000

    Update Sample.scala

--- Sample.scala
+++ Sample.scala
@@ -1,20 +1,22 @@
-object Sample {
+object Sample extends App {
 
   val busConfig = SBusConfig(
     rootUri=".servicebus.windows.net", 
     namespace="yourServiceNamspace",
     queueName="queueName",
     sasKeyName="RootManageSharedAccessKey", 
-    sasKey="yourKey=")
+    sasKey="yourKey")
     
   val incomingService = new ServiceBusClient(busConfig)
-    
-  /** recursively receive & handle messages from endpoint until no more available */  
-  def receiveMessages(handler: (String) => Unit): Unit =
+  
+  def handler = println  
+  
+  /** continually receive & handle messages from endpoint, until no more available */  
+  def receiveMessages(): Unit =
     incomingService.receive map { message =>
         handler(message)
-        receiveMessages(client, handler)
+        receiveMessages(handler)
     }
   
-  receiveMessages(incomingService, (msg: String) => println(msg))
+  receiveMessages()
 }
