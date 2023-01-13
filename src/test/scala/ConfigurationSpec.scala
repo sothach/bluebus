4b23bc1537ae5e11ca1273ed486e5694f01b12b7
Rich Birch <ringo@albumsnaps.com>
2023-01-13 14:22:19 +0000

    DRTII-576 Add drone step to publish docker image

--- src/test/scala/ConfigurationSpec.scala
+++ src/test/scala/ConfigurationSpec.scala
@@ -1,26 +1,29 @@
 import java.net.{URI, URL}
-
 import bluebus.configuration.SBusConfig
 import configuration.SBConfig
 import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.mockito.MockitoSugar
-import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.BeforeAndAfterAll
 
 
-class ConfigurationSpec extends FlatSpec with Matchers with ScalaFutures with MockitoSugar with BeforeAndAfterAll {
+class ConfigurationSpec extends AnyWordSpec with Matchers with ScalaFutures with BeforeAndAfterAll {
 
-  "when the SBusConfig is created, it" should
+  "when the SBusConfig is created, it" should {
     "return the expected endpoint" in {
 
-    val subject = SBusConfig(SBConfig.config)
+      val subject = SBusConfig(SBConfig.config)
 
-    subject.endpoint shouldBe "http://127.0.0.1/test/messages"
+      subject.endpoint shouldBe "http://127.0.0.1/test/messages"
+    }
   }
 
-  "when the SBusConfig is called with an invalid queue name" should "fail the requirement" in {
-    the [IllegalArgumentException] thrownBy {
-      SBusConfig(rootUri=new URL("http://localhost"), queueName="", sasKeyName="", sasKey="")
-    } should have message "requirement failed: Queue name must be specified"
+  "when the SBusConfig is called with an invalid queue name" should {
+    "fail the requirement" in {
+      the [IllegalArgumentException] thrownBy {
+        SBusConfig(rootUri=new URL("http://localhost"), queueName="", sasKeyName="", sasKey="")
+      } should have message "requirement failed: Queue name must be specified"
+    }
   }
 
 }
